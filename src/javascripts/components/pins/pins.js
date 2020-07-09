657001db1c6f34e64cccf078446af4c4f1b8eb85
Ryan Beiden <rdbeiden@gmail.com>
2020-07-08 19:24:09 -0500

    add function to display each board's pins I decided to loop through the pins from firebase and if the id of the click event from selecting a board (which is the board's id) is equal to the pin's board's id, then print the pin. The back to boards button also works and essentially wipes the DOM and reprints the boards again.

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,27 +1,38 @@
 import pinsData from '../../helpers/data/pinsData';
 import utils from '../../helpers/utils';
+import home from '../home/home';
 import './pins.scss';
 
-const buildPins = () => {
+const buildPins = (e) => {
+  e.preventDefault();
+
+  home.navbarSignOut('Pins');
+  $('#pins').removeClass('hide');
+
+  const boardId = e.target.closest('.board-frame').id;
   pinsData.getPins()
     .then((pins) => {
       let domString = `
         <div class="container">
+          <button class="btn btn-warning" id="back-to-boards">Back to Boards</button>
       `;
       pins.forEach((pin) => {
-        domString += `
-          <h1 class="pin-name">${pin.pinName}</h1>
-          <div class="d-flex justify-content-center">
-            <img class="pin-image" src="${pin.imageUrl}">
-          </div>
-        `;
+        if (pin.boardId === boardId) {
+          domString += `
+            <h1 class="pin-name">${pin.pinName}</h1>
+            <div class="d-flex justify-content-center">
+              <img class="pin-image" src="${pin.imageUrl}">
+            </div>
+          `;
+        } else;
       });
       domString += `
         </div>
       `;
       utils.printToDom('#pins', domString);
+      utils.printToDom('#boards', '');
     })
-    .catch((err) => console.error('Getting the pins did not work - ', err));
+    .catch((err) => console.error('Getting the pins did not work -> ', err));
 };
 
 export default { buildPins };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,11 +5,18 @@ import authData from './helpers/data/authData';
 import home from './components/home/home';
 import boards from './components/boards/boards';
 
+const clickEvents = () => {
+  $('body').on('click', '#back-to-boards', boards.buildBoards);
+  $('body').on('click', '#logout-button', home.signMeOut);
+  $('body').on('click', '#login-button', home.signMeIn);
+};
+
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   home.navbarSignIn();
   authData.checkLoginStatus();
-  boards.navbarSignOut();
+  home.navbarSignOut('Boards');
+  clickEvents();
 };
 
 init();
