23f440330c6658500bde0b91f7a0f5cba2d3268d
Ryan Beiden <rdbeiden@gmail.com>
2020-07-09 13:00:44 -0500

    add delete button for boards and click event to find board's id

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -10,6 +10,21 @@ const buildBoardsPins = (e) => {
   pins.buildPins(boardId);
 };
 
+const deleteBoard = (e) => {
+  const deleteId = e.target.classList[0];
+  console.warn(deleteId);
+  // boardsData.deleteBoard(deleteId)
+  //   .then(() => {
+  //     boardsData.getBoards()
+  //       .then(() => {
+  //         // eslint-disable-next-line no-use-before-define
+  //         buildBoards();
+  //       })
+  //       .catch((err) => console.warn('getting new pins did not work -> ', err));
+  //   })
+  //   .catch((err) => console.error('Deleting this pin did not work -> ', err));
+};
+
 const buildBoards = () => {
   home.navbarSignOut('Boards');
   utils.printToDom('#pins', '');
@@ -18,12 +33,17 @@ const buildBoards = () => {
       let domString = '<div class="d-flex justify-content-center mt-5">';
       boards.forEach((board) => {
         domString += `
-          <div class="board-frame" id="${board.id}">
-            <h1 class="board-pin-name">${board.boardName}</h1>
-            <button class="btn delete-board btn-danger" id="${board.id}"><i class="fas fa-times-circle"></i> Delete Board</button>
+          <div>
+            <div class="board-frame" id="${board.id}">
+              <h1 class="board-pin-name">${board.boardName}</h1>
+            </div>
+            <div class="d-flex justify-content-center">
+              <button class="${board.id} btn delete-board btn-secondary"><i class="fas fa-times"></i> Delete Board</button>
+            </div>
           </div>
         `;
         $('body').on('click', `#${board.id}`, buildBoardsPins);
+        $('body').on('click', `.${board.id}`, deleteBoard);
       });
       domString += '</div>';
       utils.printToDom('#boards', domString);
