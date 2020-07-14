a2752e8bbb4f9e3b27a5180bfb5dcfe9767b7cbb
Ryan Beiden <rdbeiden@gmail.com>
2020-07-14 17:24:13 -0400

    wip / add edit dropdown form & update axios request currently trying to update the board a pin is on through a dropdown form. I managed to get the form to dropdown when clicking the edit button for each pin. My current issue is correctly pulling each board as an option for the radio button. I feel like an issue may be with how I am looping through the boards array while inside another `forEach` for the pins. It could be as simple as some misplaced curly braces or parenthesis, but I am so stumped and need to step away.

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 import pinsData from '../../helpers/data/pinsData';
+import boardsData from '../../helpers/data/boardsData';
 import utils from '../../helpers/utils';
 import home from '../home/home';
 import newPin from '../newPin/newPin';
@@ -69,6 +70,22 @@ const deletePin = (e) => {
     .catch((err) => console.error('deleting this pin did not work -> ', err));
 };
 
+// WIP: currently trying to update the board a pin is on through a dropdown form
+// I managed to get the form to dropdown when clicking the edit button for each pin
+// My current issue is correctly pulling each board as an option for thr radio button.
+// I feel like an issue may be with how I am looping through the boards array while inside another
+// `forEach` for the pins. It could be as simple as some misplaced curly braces or parenthesis, but
+// I am so stumped and need to step away.
+
+const editPinEvent = (e) => {
+  e.preventDefault();
+
+  const editId = e.target.closest('button').dataset.editPinId;
+  const { boardId } = e.currentTarget.dataset;
+
+  console.warn(editId, boardId);
+};
+
 const buildPins = (boardId) => {
   home.navbarSignOut('Pins');
   $('#pins').removeClass('hide');
@@ -86,9 +103,36 @@ const buildPins = (boardId) => {
               <button class="btn delete-pin" id="${pin.id}" data-board-id=${boardId}><i class="fas fa-times-circle"></i></button>
               <h1 class="pin-name">${pin.pinName}</h1>
               <image class="pin-image" src="${pin.imageUrl}">
+              <div class="d-flex justify-content-end">
+                <div class="btn-group dropleft">
+                <button type="button" class="dropdown-toggle btn edit-pin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-edit"></i></button>
+                  <div class="dropdown-menu edit-pin-form">
+                    <form>
+                      <div class="form-group">
+                        <label for="custom-pin-name">Which board should "${pin.pinName}" belong too?</label>
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons">`;
+          boardsData.getBoards()
+            .then((allBoards) => {
+              allBoards.forEach((eachBoard) => {
+                domString += `
+                <label class="btn btn-secondary active">
+                  <input type="radio" name="options" id="option1" autocomplete="off" checked> ${eachBoard.boardName}
+                </label>
+              `;
+              });
+            });
+          domString += `
+                        </div>
+                      </div>
+                      <button type="submit" class="btn btn-danger update-pin" id="update-pin" data-board-id=${boardId} data-edit-pin-id=${pin.id}>Update</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
           `;
           $('body').one('click', `#${pin.id}`, deletePin);
+          $('body').one('click', '#update-pin', editPinEvent);
         } else;
       });
       domString += `
