61cc49d1b3044aa3c514f4d5e0b3de893b3aba45
Ryan Beiden <rdbeiden@gmail.com>
2020-07-23 17:26:27 -0500

    each uid now has its own boards

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -72,18 +72,20 @@ const buildBoards = () => {
     .then((boards) => {
       let domString = '<div class="d-flex justify-content-center mt-5 flex-wrap">';
       boards.forEach((board) => {
-        domString += `
-          <div>
-            <div class="board-frame" id="${board.id}">
-              <h1 class="board-pin-name">${board.boardName}</h1>
+        if (firebase.auth().currentUser.uid === board.uid) {
+          domString += `
+            <div>
+              <div class="board-frame" id="${board.id}">
+                <h1 class="board-pin-name">${board.boardName}</h1>
+              </div>
+              <div class="d-flex justify-content-center">
+                <button class="${board.id} btn delete-board btn-secondary"><i class="fas fa-times"></i> Delete Board</button>
+              </div>
             </div>
-            <div class="d-flex justify-content-center">
-              <button class="${board.id} btn delete-board btn-secondary"><i class="fas fa-times"></i> Delete Board</button>
-            </div>
-          </div>
-        `;
-        $('body').on('click', `#${board.id}`, buildBoardsPins);
-        $('body').on('click', `.${board.id}`, deleteBoard);
+          `;
+          $('body').on('click', `#${board.id}`, buildBoardsPins);
+          $('body').on('click', `.${board.id}`, deleteBoard);
+        }
       });
       domString += '</div>';
       utils.printToDom('#boards', domString);
